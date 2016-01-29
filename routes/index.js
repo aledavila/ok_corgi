ba711a7cd011c407e2dc93f3da8c0c8330b347fc
Samuel Ryan Waits <ryanwaits@Samuels-MacBook-Air.local>
2016-01-29 14:29:03 -0600

    add shuffle function for dogs

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Dog = require('../models/dog');
 
+function shuffle(o){
+   for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+   return o;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   Dog.find({}, 'name age image aboutMe', function(err, dog) {
     if (err) console.log(err);
+
+    dog = shuffle(dog);
 
     res.render('index', {
       dogs: dog
