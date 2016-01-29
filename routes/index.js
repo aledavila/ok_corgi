74de8048a87496906b5ec8655e3edc0d3e272e04
Alessandra Davila <davilaaguilar23@gmail.com>
2016-01-29 14:02:51 -0600

    deletes whitepaces

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,14 @@ var Dog = require('../models/dog')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
+
   Dog.find({}, 'name age image aboutMe', function(err, dog) {
     if (err) console.log(err);
-    
+
     res.render('index', {
       dogs: dog
     });
-
-
   });
-
-
 });
 
 module.exports = router;
