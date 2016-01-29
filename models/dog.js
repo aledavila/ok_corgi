c938e7be03113f76ebbde66770e89ffdbec3aa3c
Alessandra Davila <davilaaguilar23@gmail.com>
2016-01-29 10:24:21 -0600

    adds schema

--- models/dog.js
+++ models/dog.js
@@ -1,1 +1,16 @@
+var mongoose = require('mongoose');
+
+// var Schema = mongoose.Schema;
+
+var corgiSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: String, required: true },
+  image: { type: String, required: true }
+
+});
+
+
+var Corgi = mongoose.model('Corgi', corgiSchema);
+// Make this available to our other files
+module.exports = Compliment;
 // Dog model w/ Mongoose
